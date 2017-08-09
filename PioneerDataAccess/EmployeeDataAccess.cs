f8aac956abc5a4efc57aaa5865e2f187fe3083eb
21rakesh <30535415+21rakesh@users.noreply.github.com>
2017-08-08 19:14:59 -0500

    new commit

---
+++ PioneerDataAccess/EmployeeDataAccess.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace PioneerDataAccess
+{
+    public class EmployeeDataAccess
+    {
+        public int SaveEmployee(string First_Name, string Last_Name, string Email, long Mobile_Number, long AlternateMobileNumber,
+            string Address1, string Address2, string Current_Country, string Home_Country, int ZipCode)
+        {
+            int result = 0;
+            try
+            {
+                string connectionstring = "Data Source=RAKI;Initial Catalog=PioneerEmployeeDB;" + "Integrated Security=True";
+                SqlConnection mysqlconnection = new SqlConnection(connectionstring);
+                mysqlconnection.Open();
+                string sqlemployeedetails = @"INSERT INTO Employee_Details
+                                            (First_Name,Last_Name,Email,Mobile_Number,AlternateMobileNumber,Address1,Adress2,Current_Country,Home_Country,ZipCode)VALUES('" + First_Name + "'," + "" + "'" + Last_Name + "'," + "" + "'" + Email + "'," + "" + "" + Mobile_Number + "," + "" + AlternateMobileNumber + "," + "" + "'" + Address1 + "'," + "'" + Address2 + "'," + "" + "" + "'" + Current_Country + "'," + "'" + Home_Country + "'," + "" + ZipCode + ")";
+                SqlCommand command;
+                command = new SqlCommand(sqlemployeedetails, mysqlconnection);
+                result = command.ExecuteNonQuery();
+                mysqlconnection.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error has been occured, please contact the administartor: " + ex.Message);
+            }
+            return result;
+
+        }
+    }
+    public class EducationDataAccess
+    {
+        public int SaveEducation(string CourseType, string CourseSpecialisation, int YearOfPass)
+        {
+            int result = 0;
+            try
+            {
+                string connectionstring = "Data Source=RAKI;Initial Catalog=PioneerEmployeeDB;" + "Integrated Security=True";
+                SqlConnection mysqlconnection = new SqlConnection(connectionstring);
+                mysqlconnection.Open();
+                string sqleducationdetails = @"INSERT INTO Education_Details(CourseType,CourseSpecialisation,YearOfPass)VALUES('" + CourseType + "'," + "'" + CourseSpecialisation + "'," + "" + YearOfPass + ")";
+                SqlCommand command;
+                command = new SqlCommand(sqleducationdetails, mysqlconnection);
+                result=command.ExecuteNonQuery();
+                mysqlconnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error has been occured, please contact the administartor: " + ex.Message);
+            }
+            return result;
+        }
+    }
+    public class TechnicalDataAccess
+    {
+        public int SaveTechnical(string UI, string Programming_Languages, string ORM_Technologies, string Databases)
+        {
+            int result = 0;
+            try
+            {
+                string connectionstring = "Data Source=RAKI;Initial Catalog=PioneerEmployeeDB;" + "Integrated Security=True";
+                SqlConnection mysqlconnection = new SqlConnection(connectionstring);
+                mysqlconnection.Open();
+                string sqltechnicaldetails = @"INSERT INTO Technical_Details(UI,Programming_Languages,ORM_Technologies,Databases)VALUES('" + UI + "'," + "'" + Programming_Languages + "'," + "'" + ORM_Technologies + "'," + "'" + Databases + "')";
+                SqlCommand command;
+                command = new SqlCommand(sqltechnicaldetails, mysqlconnection);
+                result=command.ExecuteNonQuery();
+                mysqlconnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error has been occured, please contact the administartor: " + ex.Message);
+            }
+            return result;
+        }
+    }
+    public class CompanyDataAccess
+    {
+        public int SaveCompany(string Employer_Name,long Contact_Number,string Location,string Website,string Databases)
+        {
+            int result = 0;
+            try
+            {
+                string connectionstring = "Data Source=RAKI;Initial Catalog=PioneerEmployeeDB;"+"Integrated Security=True";
+                SqlConnection mysqlconnection = new SqlConnection(connectionstring);
+                mysqlconnection.Open();
+                string sqlcompanydetails = @"INSERT INTO Company_Details(Employer_Name,Contact_Number,Location,Website)VALUES('"+Employer_Name+"',"+""+Contact_Number+"," + "'" + Location + "'," + "'" + Website + "')";
+                SqlCommand command;
+                command = new SqlCommand(sqlcompanydetails, mysqlconnection);
+                result=command.ExecuteNonQuery();
+                mysqlconnection.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("An error has been occured, please contact the administartor: " + ex.Message);
+            }
+            return result;
+        }
+    }
+    public class ProjectDataAccess
+    {
+        public int SaveProject(int EmployeeID,string Project_Name,string Client_Name,string Location,string Roles,string Databases)
+        {
+            int result = 0;
+            try
+            {
+                string connectionstring = "Data Source=RAKI;Initial Catalog=PioneerEmployeeDB;" +
+                   " Integrated Security=True";
+                SqlConnection mysqlconnection = new SqlConnection(connectionstring);
+                mysqlconnection.Open();
+                string sqlprojectdetails = @"INSERT INTO Project_Details(EmployeeID,Project_Name,Client_Name,Location,Roles)VALUES(" + EmployeeID + "," + "'" + Project_Name + "'," + "'" + Client_Name + "'," + "'" + Location + "'," + "'" + Roles + "')";
+                SqlCommand command;
+                command = new SqlCommand(sqlprojectdetails, mysqlconnection);
+                command.ExecuteNonQuery();
+                mysqlconnection.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("An error has been occured, please contact the administartor: " + ex.Message);
+            }
+            return result;
+        }
+    } 
+
+
+}
+
+
--- PioneerTestConsultancySystem/Form1.cs
+++ PioneerTestConsultancySystem/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using PioneerDataAccess;
 
 namespace PioneerTestConsultancySystem
 {
@@ -20,57 +21,25 @@ namespace PioneerTestConsultancySystem
         
         private void button1_Click(object sender, EventArgs e)
         {
-            try
-            {
             
                 string First_Name = FirstNametextbox.Text;
                 string Last_Name = LastNametextbox.Text;
                 string Email = Emailtextbox.Text;
-                var Mobile_Number = Convert.ToInt64(MobileNumbertextbox.Text);
+                long Mobile_Number = Convert.ToInt64(MobileNumbertextbox.Text);
                 long AlternateMobileNumber = Convert.ToInt64(AlternateMobileNumbertextbox.Text);
                 string Address1 = Address1textbox.Text;
                 string Address2 = Address2textbox.Text;
                 string Current_Country = CurrentCountrytextbox.Text;
                 string Home_Country = HomeCountrytextbox.Text;
                 int ZipCode = Convert.ToInt32(Zipcodetextbox.Text);
-                string connectionstring = "Data Source=RAKI;Initial Catalog=PioneerEmployeeDB;" +
-                    " Integrated Security=True";
-                SqlConnection mysqlconnection = new SqlConnection(connectionstring);
-                mysqlconnection.Open();
-                string sqlemployeedetails= @"INSERT INTO Employee_Details
-                                            (First_Name,
-                                            Last_Name,
-                                            Email,
-                                            Mobile_Number,
-                                            AlternateMobileNumber,
-                                            Address1,
-                                            Adress2,
-                                            Current_Country,
-                                            Home_Country,
-                                            Zipcode
-                                            )
-                                            VALUES
-                                            ('"+First_Name+"'," +
-                                            ""+"'"+Last_Name+"'," +
-                                            ""+"'"+Email+"'," +
-                                            ""+Mobile_Number+"," +
-                                            ""+AlternateMobileNumber+"," +
-                                            ""+"'"+Address1+"'," +
-                                            ""+"'"+Address2+"'," +
-                                            ""+"'"+Current_Country+"'," +
-                                            ""+"'"+Home_Country+"'," +
-                                            ""+ZipCode+")";
-                SqlCommand command;
-                command = new SqlCommand(sqlemployeedetails, mysqlconnection);
-                command.ExecuteNonQuery();
-                mysqlconnection.Close();
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show("An error has been occured, please contact the administartor: " + ex.Message);
-            }            
+            EmployeeDataAccess employee = new EmployeeDataAccess();
+            employee.SaveEmployee(First_Name,Last_Name,Email,Mobile_Number,AlternateMobileNumber,Address1,Address2,
+                Current_Country,Home_Country,ZipCode);
+            
+
+
         }
- 
+
         private void Clear_Click(object sender, EventArgs e)
         {
             FirstNametextbox.Text=String.Empty;
@@ -105,7 +74,6 @@ namespace PioneerTestConsultancySystem
                 gridview.DataSource = employeeData;
                 EmployeeDetails.DataSource = gridview;
                 employeeData.Close();
-               // string r = ("SELECT * FROM Education_Details WHERE EmployeeID=" + EmployeeID);
                 
                 command = new SqlCommand(sqlEducationDetails, mysqlconnection);
                 SqlDataReader eduacationaldata = command.ExecuteReader();
@@ -113,18 +81,21 @@ namespace PioneerTestConsultancySystem
                 educationalgridview.DataSource = eduacationaldata;
                 EducationalDetails.DataSource = educationalgridview;
                 eduacationaldata.Close();
+
                 command = new SqlCommand(sqlTechnicalDetails, mysqlconnection);
                 SqlDataReader technicaldata= command.ExecuteReader();
                 BindingSource technicalgridview = new BindingSource();
                 technicalgridview.DataSource = technicaldata;
                 TechnicalDetails.DataSource = technicalgridview;
                 technicaldata.Close();
+
                 command = new SqlCommand(sqlCompanyDetails, mysqlconnection);
                 SqlDataReader companydata = command.ExecuteReader();
                 BindingSource companygridview= new BindingSource();
                 companygridview.DataSource = companydata;
                 CompanyDetails.DataSource = companygridview;
                 companydata.Close();
+
                 command = new SqlCommand(sqlProjectDetails, mysqlconnection);
                 SqlDataReader  projectdata= command.ExecuteReader();
                 BindingSource projectgridview = new BindingSource();
@@ -141,103 +112,47 @@ namespace PioneerTestConsultancySystem
 
         private void button4_Click(object sender, EventArgs e)
         {
-            try
-            {
             string CourseType = CourseTypetextbox.Text;
             string CourseSpecialisation = CourseSpecialisationtextbox.Text;
-            string YearOfPass = YearOfPasstextbox.Text;
-            string connectionstring = "Data Source=RAKI;Initial Catalog=PioneerEmployeeDB;" +
-                   " Integrated Security=True";
-            SqlConnection mysqlconnection = new SqlConnection(connectionstring);
-            mysqlconnection.Open();
-                string sqleducationdetails = @"INSERT INTO Education_Details(CourseType,CourseSpecialisation,YearOfPass)VALUES('"+CourseType+"',"+"'"+CourseSpecialisation+"',"+"'"+YearOfPass+"')";
-            SqlCommand command;
-            command = new SqlCommand(sqleducationdetails, mysqlconnection);
-            command.ExecuteNonQuery();
-            mysqlconnection.Close();
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show("An error has been occured, please contact the administartor: " + ex.Message);
-            }
-           
+            int YearOfPass = Convert.ToInt32(YearOfPasstextbox.Text);
+            EducationDataAccess education = new EducationDataAccess();
+            education.SaveEducation(CourseType, CourseSpecialisation, YearOfPass);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            try
-            {
             string UI = UItextbox.Text;
             string Programming_Languages = Programmingtextbox.Text;
             string ORM_Technologies = ORMtextbox.Text;
             string Databases = Databasestextbox.Text;
-            string connectionstring = "Data Source=RAKI;Initial Catalog=PioneerEmployeeDB;" +
-                   " Integrated Security=True";
-            SqlConnection mysqlconnection = new SqlConnection(connectionstring);
-            mysqlconnection.Open();
-            string sqltechnicaldetails = @"INSERT INTO Technical_Details(UI,Programming_Languages,ORM_Technologies,Databases)VALUES('"+UI+"',"+"'"+Programming_Languages+"',"+"'"+ORM_Technologies+"',"+"'"+Databases+"')";
-            SqlCommand command;
-            command = new SqlCommand(sqltechnicaldetails, mysqlconnection);
-            command.ExecuteNonQuery();
-            mysqlconnection.Close();
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show("An error has been occured, please contact the administartor: " + ex.Message);
-            }
+            TechnicalDataAccess technical = new TechnicalDataAccess();
+            technical.SaveTechnical(UI, Programming_Languages, ORM_Technologies, Databases);
            
         }
 
         private void button9_Click(object sender, EventArgs e)
-        {
-            try
-            {
+        { 
             string Employer_Name = Employertextbox.Text;
-            long Contact_NUmber = Convert.ToInt64(ContactNumbertextbox.Text);
+            long Contact_Number = Convert.ToInt64(ContactNumbertextbox.Text);
             string Location = Locationtextbox.Text;
-            var Website = Websitetextbox.Text;
+            string Website = Websitetextbox.Text;
             string Databases = Databasestextbox.Text;
-            string connectionstring = "Data Source=RAKI;Initial Catalog=PioneerEmployeeDB;" +
-                   " Integrated Security=True";
-            SqlConnection mysqlconnection = new SqlConnection(connectionstring);
-            mysqlconnection.Open();
-            string sqlcompanydetails = @"INSERT INTO Company_Details(Employer_Name,Contact_Number,Location,Website)VALUES('"+Employer_Name+"',"+""+Contact_NUmber+"," + "'"+Location+"',"+"'"+Website+"')";
-            SqlCommand command;
-            command = new SqlCommand(sqlcompanydetails, mysqlconnection);
-            command.ExecuteNonQuery();
-            mysqlconnection.Close();
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show("An error has been occured, please contact the administartor: " + ex.Message);
-            }
+            CompanyDataAccess company = new CompanyDataAccess();
+            company.SaveCompany(Employer_Name, Contact_Number, Location, Website, Databases);
            
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            try
-            {
+           
             int EmployeeID = Convert.ToInt32(EmployeeID1textbox.Text);
             string Project_Name = ProjectNametextbox.Text;
             string Client_Name = ClientNametextbox.Text;
             string Location = ProjectLocationtextbox.Text;
             string Roles = Roletextbox.Text;
             string Databases = Databasestextbox.Text;
-            string connectionstring = "Data Source=RAKI;Initial Catalog=PioneerEmployeeDB;" +
-                   " Integrated Security=True";
-            SqlConnection mysqlconnection = new SqlConnection(connectionstring);
-            mysqlconnection.Open();
-            string sqlprojectdetails = @"INSERT INTO Project_Details(EmployeeID,Project_Name,Client_Name,Location,Roles)VALUES("+EmployeeID+","+"'"+Project_Name+"',"+"'"+Client_Name+"',"+"'"+Location+"',"+"'"+Roles+"')";
-            SqlCommand command;
-            command = new SqlCommand(sqlprojectdetails, mysqlconnection);
-            command.ExecuteNonQuery();
-            mysqlconnection.Close();
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show("An error has been occured, please contact the administartor: " + ex.Message);
-            }
+            ProjectDataAccess project = new ProjectDataAccess();
+            project.SaveProject(EmployeeID, Project_Name, Client_Name, Location, Roles, Databases);
             
         }
 
